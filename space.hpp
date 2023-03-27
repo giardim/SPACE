29ff8398f533580e3f482603dcfc1568513a7a58
Michael Giardina <mgiardina02@gmail.com>
2023-03-27 15:42:48 -0400

    Added Data Structures to Sout

--- space.hpp
+++ space.hpp
@@ -5,7 +5,9 @@
 #include <fstream>
 #include <map>
 #include <sstream>
-#include <string.h>
+#include <stack>
+#include <queue>
+
 
 namespace space
 {
@@ -341,6 +343,223 @@ namespace space
     data[name]["data"] += "      " + str.str() + ",\n";
     data[name]["indices"] += "      " + indices.str() + ",\n";
   }
+
+  void sout (std::string name, std::string &type, double array[], int size){
+    std::string fixedType = "";
+    for (int i = 0; i < type.length(); ++i){
+      fixedType += char(std::tolower(type[i]));
+      }
+
+  
+    while (fixedType != "stack" && fixedType != "queue" && fixedType != "dequeue"){
+      fixedType = "";
+      type = "";
+      std::cout << "You did not enter a valid data structure, please indicate if you are visualizing";
+      std::cout << "a 'stack', 'queue', or 'dequeue': ";
+      std::cin >> fixedType;
+
+      for (int i = 0; i < type.length(); ++i){
+        fixedType += char(std::tolower(type[i]));
+      }
+
+      type = fixedType;
+      std::cout << fixedType << " " << type << "\n";
+    }
+
+    data[name]["type"] = fixedType;
+    data[name]["is_char"] = "0";
+    indices.str("");
+    indices << "[" << -1 << ", " << -1 << "]";
+    str.str("");
+    str << "[";
+    for (int i = 0; i < size; ++i){
+      str << array[i] << (i < size - 1 ? ", " : "");
+    }
+    str << "]";
+    data[name]["data"] += "      " + str.str() + ",\n";
+    data[name]["indices"] += "      " + indices.str() + ",\n";
+  }
+
+  void sout (std::string name, std::string &type, char array[], int size){
+    std::string fixedType = "";
+    for (int i = 0; i < type.length(); ++i){
+      fixedType += char(std::tolower(type[i]));
+      }
+
+  
+    while (fixedType != "stack" && fixedType != "queue" && fixedType != "dequeue"){
+      fixedType = "";
+      type = "";
+      std::cout << "You did not enter a valid data structure, please indicate if you are visualizing";
+      std::cout << "a 'stack', 'queue', or 'dequeue': ";
+      std::cin >> fixedType;
+
+      for (int i = 0; i < type.length(); ++i){
+        fixedType += char(std::tolower(type[i]));
+      }
+
+      type = fixedType;
+      std::cout << fixedType << " " << type << "\n";
+    }
+
+    data[name]["type"] = fixedType;
+    data[name]["is_char"] = "1";
+    indices.str("");
+    indices << "[" << -1 << ", " << -1 << "]";
+    str.str("");
+    str << "[";
+    for (int i = 0; i < size; ++i){
+      str << (int)array[i] << (i < size - 1 ? ", " : "");
+    }
+    str << "]";
+    data[name]["data"] += "      " + str.str() + ",\n";
+    data[name]["indices"] += "      " + indices.str() + ",\n";
+  }
+
+  void sout (std::string name, std::string &type, boolean array[], int size){
+    std::string fixedType = "";
+    for (int i = 0; i < type.length(); ++i){
+      fixedType += char(std::tolower(type[i]));
+      }
+
+  
+    while (fixedType != "stack" && fixedType != "queue" && fixedType != "dequeue"){
+      fixedType = "";
+      type = "";
+      std::cout << "You did not enter a valid data structure, please indicate if you are visualizing";
+      std::cout << "a 'stack', 'queue', or 'dequeue': ";
+      std::cin >> fixedType;
+
+      for (int i = 0; i < type.length(); ++i){
+        fixedType += char(std::tolower(type[i]));
+      }
+
+      type = fixedType;
+      std::cout << fixedType << " " << type << "\n";
+    }
+
+    data[name]["type"] = fixedType;
+    data[name]["is_char"] = "0";
+    indices.str("");
+    indices << "[" << -1 << ", " << -1 << "]";
+    str.str("");
+    str << "[";
+    for (int i = 0; i < size; ++i){
+      str << array[i] << (i < size - 1 ? ", " : "");
+    }
+    str << "]";
+    data[name]["data"] += "      " + str.str() + ",\n";
+    data[name]["indices"] += "      " + indices.str() + ",\n";
+  }
+
+  void sout (std::string name, std::string &type, float array[], int size){
+    std::string fixedType = "";
+    for (int i = 0; i < type.length(); ++i){
+      fixedType += char(std::tolower(type[i]));
+      }
+
+  
+    while (fixedType != "stack" && fixedType != "queue" && fixedType != "dequeue"){
+      fixedType = "";
+      type = "";
+      std::cout << "You did not enter a valid data structure, please indicate if you are visualizing";
+      std::cout << "a 'stack', 'queue', or 'dequeue': ";
+      std::cin >> fixedType;
+
+      for (int i = 0; i < type.length(); ++i){
+        fixedType += char(std::tolower(type[i]));
+      }
+
+      type = fixedType;
+      std::cout << fixedType << " " << type << "\n";
+    }
+
+    data[name]["type"] = fixedType;
+    data[name]["is_char"] = "0";
+    indices.str("");
+    indices << "[" << -1 << ", " << -1 << "]";
+    str.str("");
+    str << "[";
+    for (int i = 0; i < size; ++i){
+      str << array[i] << (i < size - 1 ? ", " : "");
+    }
+    str << "]";
+    data[name]["data"] += "      " + str.str() + ",\n";
+    data[name]["indices"] += "      " + indices.str() + ",\n";
+  }
+
+  void sout (std::string name, std::string &type, wchar_t array[], int size){
+    std::string fixedType = "";
+    for (int i = 0; i < type.length(); ++i){
+      fixedType += char(std::tolower(type[i]));
+      }
+
+  
+    while (fixedType != "stack" && fixedType != "queue" && fixedType != "dequeue"){
+      fixedType = "";
+      type = "";
+      std::cout << "You did not enter a valid data structure, please indicate if you are visualizing";
+      std::cout << "a 'stack', 'queue', or 'dequeue': ";
+      std::cin >> fixedType;
+
+      for (int i = 0; i < type.length(); ++i){
+        fixedType += char(std::tolower(type[i]));
+      }
+
+      type = fixedType;
+      std::cout << fixedType << " " << type << "\n";
+    }
+
+    data[name]["type"] = fixedType;
+    data[name]["is_char"] = "1";
+    indices.str("");
+    indices << "[" << -1 << ", " << -1 << "]";
+    str.str("");
+    str << "[";
+    for (int i = 0; i < size; ++i){
+      str << (int)array[i] << (i < size - 1 ? ", " : "");
+    }
+    str << "]";
+    data[name]["data"] += "      " + str.str() + ",\n";
+    data[name]["indices"] += "      " + indices.str() + ",\n";
+  }
+
+  void sout (std::string name, std::stack<int> stack){
+    int initSize = stack.size();
+    data[name]["type"] = "stack";
+    data[name]["is_char"] = "1";
+    indices.str("");
+    indices << "[" << -1 << ", " << -1 << "]";
+    str.str("");
+    str << "[";
+    for (int i = 0; i < initSize; i++)
+    {
+      str << stack.top() << (i < initSize - 1 ? ", " : "");
+      stack.pop();
+    }
+    str << "]";
+    data[name]["indices"] += "      " + indices.str() + ",\n";
+    data[name]["data"] += "      " + str.str() + ",\n";
+  }
+
+  void sout (std::string name, std::queue<int> queue){
+    int initSize = queue.size();
+    data[name]["type"] = "queue";
+    data[name]["is_char"] = "1";
+    indices.str("");
+    indices << "[" << -1 << ", " << -1 << "]";
+    str.str("");
+    str << "[";
+    for (int i = 0; i < initSize; i++)
+    {
+      str << queue.front() << (i < initSize - 1 ? ", " : "");
+      queue.pop();
+    }
+    str << "]";
+    data[name]["indices"] += "      " + indices.str() + ",\n";
+    data[name]["data"] += "      " + str.str() + ",\n";
+  }
+
 }
 
 
