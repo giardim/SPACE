522685fc070f52a90edc606efc47c8ada7fdcbc6
Michael Giardina <mgiardina02@gmail.com>
2023-04-21 11:45:47 -0400

    Added User Examples

---
+++ UserExamples/queues_examples.cpp
@@ -0,0 +1,44 @@
+/*
+This program is to show the user how to use our product to save and store
+    STL queues
+*/
+
+#include <iostream>
+#include <queue>
+
+#include "../space.hpp"
+
+int main(void)
+{
+    // Below are initializations of queues
+    std::queue<int> int_queue;
+    std::queue<float> float_queue;
+    std::queue<double> double_queue;
+    std::queue<bool> bool_queue;
+    std::queue<char> char_queue;
+
+    // Below is a loop to fill the queues with values
+    for (int i = 97; i < 102; ++i)
+    {
+        int_queue.push(i);
+        float_queue.push(i + .5);
+        double_queue.push(i + .8);
+        bool_queue.push(i);
+        char_queue.push(i);
+    }
+
+    // Now that the queues are full, we can call our function to save the data into a JSON file
+    // Note: for queues, our sout function takes two parameters, the name of the array as a string
+    //      and the queue itself
+    space::sout("int_queue", int_queue);
+    space::sout("float_queue", float_queue);
+    space::sout("double_queue", double_queue);
+    space::sout("bool_queue", bool_queue);
+    space::sout("char_queue", char_queue);
+
+    // Notice how a data.JSON file has either been created or overwritten,
+    //      you can now drag and drop that data.JSON file into the browser app
+    //      to visualize your queue
+
+    return 0;
+}
---
+++ UserExamples/stacks_examples.cpp
@@ -0,0 +1,44 @@
+/*
+This program is to show the user how to use our product to save and store
+    STL stacks
+*/
+
+#include <iostream>
+#include <stack>
+
+#include "../space.hpp"
+
+int main(void)
+{
+    // Below are initializations of stacks
+    std::stack<int> int_stack;
+    std::stack<float> float_stack;
+    std::stack<double> double_stack;
+    std::stack<bool> bool_stack;
+    std::stack<char> char_stack;
+
+    // Below is a loop to fill the stacks with values
+    for (int i = 97; i < 102; ++i)
+    {
+        int_stack.push(i);
+        float_stack.push(i + .5);
+        double_stack.push(i + .8);
+        bool_stack.push(i);
+        char_stack.push(i);
+    }
+
+    // Now that the stacks are full, we can call our function to save the data into a JSON file
+    // Note: for stacks, our sout function takes two parameters, the name of the array as a string
+    //      and the stack itself
+    space::sout("int_stack", int_stack);
+    space::sout("float_stack", float_stack);
+    space::sout("double_stack", double_stack);
+    space::sout("bool_stack", bool_stack);
+    space::sout("char_stack", char_stack);
+
+    // Notice how a data.JSON file has either been created or overwritten,
+    //      you can now drag and drop that data.JSON file into the browser app
+    //      to visualize your stack
+
+    return 0;
+}
